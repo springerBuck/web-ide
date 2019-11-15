0980410ba643099f983f8ade48a950673159a5a0
Aaron Buckley <aaron.buckley@springernature.com>
2019-11-15 11:54:02 +0000

    start to build out the ide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ide';
+  form: FormGroup;
+
+  get code() {
+    return this.form.get('code');
+  }
+
+  run() {
+    eval(this.code.value)
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      code: new FormControl('')
+    });
+
+  }
+
+  public setBasicOne() {
+    this.code.setValue('console.log(\'here\');')
+  }
+
+  public setBasicTwo() {
+    this.code.setValue(`
+    const a = 1234;
+    const b = 4321;
+    const c = a + b;
+    console.log(c);
+    `)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditorComponent } from './components/editor/editor.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatGridListModule, MatInputModule} from "@angular/material";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

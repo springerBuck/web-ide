4eca40d9dc2e7d429ba355df19d9fb64c38dd9bb
Aaron Buckley <aaron.buckley@springernature.com>
2019-11-29 15:17:12 +0000

    finally, a  working mvp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,52 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'ide';
-  jsEditorOptions;
-  htmlEditorOptions;
-  code: string;
-  markup: string;
-  public menuOpen: boolean = false;
+	title = 'ide';
+	code: string;
+	markup: string;
+	public menuOpen: boolean = false;
 
-  run() {
-    const frame = document.querySelector<HTMLIFrameElement>('#env');
-    frame.srcdoc = `
+	run() {
+		const frame = document.querySelector<HTMLIFrameElement>('#env');
+		frame.srcdoc = `
     <html lang="en">
-      <head>
-      <title>some title</title>
-      </head>
-      <body>
-        <div id="main"></div>
-      </body>
+		<head>
+		<title>some title</title>
+		</head>
+		<body>
+			<div id="main"></div>
+		</body>
+		<script type="application/javascript">
+				${this.code}
+		</script>
     </html>
     `;
-    const script = frame.contentDocument.createElement('script');
-    script.setAttribute('type', 'application/javascript');
-    script.textContent = this.code;
-    frame.contentDocument.body.appendChild(script);
-    console.log(frame.contentDocument.body);
-  }
+	}
 
-  ngOnInit(): void {
-    this.jsEditorOptions = {theme: 'vs-dark', language: 'javascript'};
-    this.htmlEditorOptions = {theme: 'vs-dark', language: 'html'};
-    this.setBasicThree()
-  }
+	ngOnInit(): void {
+		this.setBasicThree()
+	}
 
-  public setBasicOne() {
-    this.code = 'console.log(\'here\');';
-  }
+	public setBasicOne() {
+		this.code = 'console.log(\'here\');';
+	}
 
-  public setBasicTwo() {
-    this.code = `
+	public setBasicTwo() {
+		this.code = `
     const a = 1234;
     const b = 4321;
     const c = a + b;
     console.log(c);
     `;
-  }
+	}
 
-  public setBasicThree() {
-    this.code = `
+	public setBasicThree() {
+		this.code = `
       var div = document.createElement("div");
       div.style.width = "100px";
       div.style.height = "100px";
@@ -61,9 +55,9 @@ export class AppComponent implements OnInit {
       div.innerHTML = "Hello";
       
       document.getElementById("main").appendChild(div);`;
-  }
+	}
 
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
-  }
+	toggleMenu() {
+		this.menuOpen = !this.menuOpen;
+	}
 }
--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, forwardRef, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
-  selector: 'app-editor',
-  templateUrl: './editor.component.html',
-  styleUrls: ['./editor.component.scss']
+	selector: 'app-editor',
+	templateUrl: './editor.component.html',
+	styleUrls: ['./editor.component.scss'],
+	providers: [
+		{
+			provide: NG_VALUE_ACCESSOR,
+			useExisting: forwardRef(() => EditorComponent),
+			multi: true
+		}
+	]
 })
-export class EditorComponent implements OnInit {
+export class EditorComponent implements ControlValueAccessor, OnInit {
+	private _code: string;
+	jsEditorOptions;
+	private propagateChange: any;
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit() {
-  }
+	ngOnInit(): void {
+		this.jsEditorOptions = {theme: 'hc-black', language: 'javascript'};
+	}
 
+
+	get code(): string {
+		return this._code;
+	}
+
+	set code(value: string) {
+		this._code = value;
+
+		if (this.propagateChange) {
+			this.propagateChange(this._code);
+		}
+	}
+
+	registerOnChange(fn: any): void {
+		this.propagateChange = fn;
+	}
+
+	registerOnTouched(fn: any): void {
+	}
+
+	setDisabledState(isDisabled: boolean): void {
+	}
+
+	writeValue(value: any): void {
+		this.code = value;
+	}
 }
